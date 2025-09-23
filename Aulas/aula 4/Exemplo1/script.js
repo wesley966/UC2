ec0cfd6ab807c71f3dce3a91039b6cfda7201bc1
wesley966 <wesleitdssenac@gmail.com>
2025-09-23 19:50:49 -0300

    adicionado a pasta aulas

---
+++ Aulas/aula 4/Exemplo1/script.js
@@ -0,0 +1,20 @@
+/* Exemplo 1*/
+
+/*Wesllei*/
+
+
+
+console.log("Olá Mundo Turma 25-2N")
+
+console.log("Segundo Olá Mundo")
+
+prompt("Qual é o seu nome?")
+
+console.log("Um meteoro atingiu a Terra há milhoes de anos atrás")
+
+
+const nome = "Wesllei"
+const idade = "20"
+
+console.log("O nome é:", nome, "e sua idade é:", idade)
+
---
+++ Aulas/aula 4/Exercícios/script.js
@@ -0,0 +1,9 @@
+const nome = "Wesllei"
+
+const sobrenome = "Rosa"
+
+const idade = 20
+
+let estudante = true
+let NaoEstudante = false
+
