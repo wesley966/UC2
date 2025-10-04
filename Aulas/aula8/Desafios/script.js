310e987990f931920bc7ababa00e7f4dcd78811e
wesley966 <wesleitdssenac@gmail.com>
2025-10-03 21:45:37 -0300

    movendo para a pasta UC2

---
+++ Aulas/aula8/Desafios/script.js
@@ -0,0 +1,72 @@
+//DESAFIO 1
+//a)
+const fGraus = 77
+const kelvin = (fGraus - 32*(5/9)+273.15)
+console.log(kelvin)
+
+//b)
+const celsios = 80
+const fahrenheit =(80*(9/5)+32)
+console.log(fahrenheit)
+
+//c)
+const celsiod2 = 30
+const fahrenheit2 = (30*(9/3)+32) 
+const kelvin2 = 
+
+
+//DESAFIO 2
+const quilowattshora = 0.05
+const quilowattsResidencia = 280
+const resultado = (quilowattshora*quilowattsResidencia)
+//a)
+console.log(quilowattshora*quilowattsResidencia)
+
+//b)
+console.log(resultado%15)
+
+//DESAFIO 3
+
+//a)
+const libras = 20
+const quilograma = 0,45359237
+const resultado2 = libras*0,45359237
+console.log(libras"lb equivalem a "resultado2)
+
+//b)
+const onza = 10.5
+const quilograma = 0,02834952
+const resultado3 =onza*quilograma
+console.log(onza"oz equivalem a" resultado3)
+
+//c)
+const milha = 100
+const metro = 1609,344
+const resultado4 = milha*metro
+console.log(milha"mi equivalem a "resultado4)
+
+//d)
+const pes =50
+const metros = 0,3048
+const resultado5 = pes*metros
+console.log(pes"ft equivalem a "resultados5)
+
+//e)
+const galao = 103.5
+const litro = 3,78541
+const = resultado6 = galao*litro
+console.log(galao"gal equivalem a "resultado6)
+
+//f)
+const xicara = 450
+const litro = 0,24
+const resultado7 = xicara*litro
+console.log(xicara"xic equivalem a"resultado7)
+
+//g)
+const uniOriginal = prompt("Qual o valor origial de xicara?")
+console.log(uniOriginal)
+const litro = 0,24
+const resultado8 = uniOriginal*litro
+console.log(uniOriginal"equivalem a "resultado8)
+
--- Aulas/aula8/Exercicios de Fixacao/script.js
+++ Aulas/aula8/Exercicios de Fixacao/script.js
@@ -1,25 +1,75 @@
+//Exercicios de interpretacao decodigo 1
+
 //a primeira variavel e verdadeira
 //segunda variavel e falsa
 //terceira variavel e o oposto da segunda
 
 //pode variar a nova variavel "resultado" que e igual a primeira variavel AND a segunda
-//
+//Imprime a alternativa a e o resultado
+
+//O resultado e igual a primeira variavel AND a segunda AND a terceira
+//Imprime a alternativa b e o resultado
+
+//O resultado e igual ao contrario do resultado AND (a primeira variavel OR a segunda variavel)
+//Imprime a alternativa c e o resultado
+
+//Imprime a alternativa d TYPEOF o resultado
 
 
 
+//Exercicio de interpretacao de codigo 2
+
+//Ha a primeira variavel com uma pergunta que pode mudar
+//Ha a segunda variavel com outra pergunta que pode mudar
+
+//Ha uma terceira variavel com a soma dessas variaveis anteriores
+
+//Imprime a soma das variaveis
+
+//Converter string para number entao a soma sera correta
 
 
 //Exercicios de escrita de codigo 1
+//a)
 const idade = prompt ("Qual a sua idade?")
+//b)
 const idadeAmigo = prompt ("Qual a idade do seu melhor amigo?")
-
+//c)
 console.log("Sua idade e maior ou menor do que seu melhor amigo?",true)
-console.log("Qual a diferenca de idade?",)
+//d)
+console.log("Qual a diferenca de idade?",false)
 
 
 //Exercicios de escrita de codigo 2
+//a)
+const numero = prompt("Insira um numero par")
+console.log(numero)
+//b)
+console.log(numero%2)
+//c)
+//Os numeros sempre serao exatos
+//d)
+//O resultado sera um numero com virgula
+
 
 
 //Exercicios de esrita de codigo 3
+const idadeAnos = prompt("Quantos anos voce tem?")
+console.log(idadeAnos)
+//a)
+console.log(idadeAnos/12)
+//b)
+console.log(idadeAnos/365)
+//c)
+console.log(idadeAnos/8760)
 
 //Exercicios de escrita de codigo 4
+const prinumero = prompt("Seu primeiro numero:")
+const segunNumero = prompt("Seu segundo numero:")
+console.log(prinumero,segunNumero)
+
+console.log(prinumero>segunNumero,true)
+console.log(prinumero=segunNumero,false)
+console.log(prinumero/segunNumero,true)
+console.log(segunNumero/prinumero,false)
+
