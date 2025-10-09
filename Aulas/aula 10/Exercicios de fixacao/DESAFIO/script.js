de4a117f75f91098cb475868717cdce6ff8519ef
wesley966 <wesleitdssenac@gmail.com>
2025-10-08 21:49:17 -0300

    adicionando atividades

---
+++ Aulas/aula 10/Exercicios de fixacao/DESAFIO/script.js
@@ -0,0 +1,11 @@
+//1.
+const frase = "Todos os dias vou ao mercado comprar abacaxi, sorvete, salada, e mostarda"
+const array = ["Todos","dias","vou","mercado","comprar","abacaxi","sorvete","salada","mostarda"]
+console.log(frase)
+console.log(array)
+
+//2.
+const array2 = ["Banana", "morango", "abacaxi", "laranja", "ameixa"]
+
+console.log(array2["abacaxi"])
+console.log(array2.length)
--- Aulas/aula 10/Exercicios de fixacao/script.js
+++ Aulas/aula 10/Exercicios de fixacao/script.js
@@ -1,9 +1,25 @@
 //1.
-//Pode variar o comando arrays
-//imprime a variavel a
+let array// cria a variavel com nome array que pode variar
+console.log('a.  ', array)//imprime a variavel array como "a. indefinido"
+
+array = null//transforma a variavel array em nula
+console.log('b,  ', array)//imprime a variavel array como nula sendo "b.  null"
+
+array = [3,4,5,6,7,8,9,10,11,12,13]//adiciona a variavel array com numeros de 3 a 13
+console.log('c.   ', array.length)//imprime o numero de caracteres na variavel array como "c. 11"
+
+let i = 0//cria a variavel i que pode variar e seu indice é 0
+console.log('d.  ', array[i])//imprime o numero correspondente da variavel array "d.  3"
+
+array[i+1] = 19//adiciona ao indice 0 "1" e reposiciona o numero inicial deste indice como 19
+console.log('e.  ', array)//imprime a variavel array atualizando-a como "e. [3,19,5,6,7,8,9,10,11,12,13]"
+
+const valor = array[i+6]//cria uma nova constante variavel "valor" e adicionando 6 a variavel "i"
+console.log('f.  ', valor)//imprime a variavel "valor" como "f. 9 "
 
 
 //2.
+
 //SUBI NUM ONIBUS EM MARROCOS
 //SUBI NUM ONIBUS EM MIRROCOS
 //27
@@ -11,7 +27,7 @@
 //3.
 const nome = prompt("Qual seu nome?")
 const email = prompt("Qual seu email?")
-console.log("O e-mail " + email + " foi cadastrado com sucesso. Seja bem vindo(a), " + nome +"!")
+console.log(` e-mail   ${email}  foi cadastrado com sucesso. Seja bem vindo(a), ${nome}!`)
 
 //4.
 const comida = [ "macarrao", "batata", "amendoim", "polenta", "arroz"]
@@ -26,7 +42,8 @@ console.log(comida[3])
 console.log(comida[4])
 //c)
 const novaComida = prompt("Qual sua comida preferida?")
-console.log(comida.replaceAll("batata", novaComida))
+let indice = 1
+comida[indice] = novaComida
 console.log(comida)
 
 //5.
@@ -48,13 +65,6 @@ const indiTarefa = prompt("Qual o indice das tarefas realizadas?")
 listaDeTarefas.splice(indiTarefa, indiTarefa)
 //f)
 console.log(listaDeTarefas)
-
-
-
-
-
-
-
 
 
 
