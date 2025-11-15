54b08a7db6c2ae562d1b59a2eaafa141dd158336
wesley966 <wesleitdssenac@gmail.com>
2025-11-14 21:46:33 -0300

    adicionando avaliacao b

---
+++ Aulas/TRABALHO AVALIATIVO B/script.js
@@ -0,0 +1,70 @@
+
+const personagemExcluido = prompt("Qual o personagem a ser excluido?")
+
+function escolherPersonagem(){
+    const personagem = prompt("Qual seu personagem?")
+    const movimento = prompt("Qual o movimento?")
+if(personagem==="Frodo"){
+const Frodo={
+    nome: "Frodo",
+    classe: "Hobit",
+    nivel: 10,
+    pontosDeVida: 10,
+    habilidades:["ponto fraco inimigo","soco"],
+    historicoDeAcoes:[]
+}
+}
+
+if(movimento==="atacar"){
+console.log(!pontosDeVida-10)
+}
+else if(movimento==="defender"){
+    console.log(!pontosDeVida+10)
+}
+else if(movimento==="habilidade especial"){
+console.log(!pontosDeVida-20)
+}
+else{
+    console.log("nao identificado")
+}
+}
+if(personagem==="legolas"){
+const Legolas={
+    nome:"Legolas",
+    classe:"Elfo",
+    nivel:20,
+    pontosDeVida:20,
+    habilidades:["flecha","super flecha"],
+    historicoDeAcoes:[]
+}
+
+    if(movimento==="atacar"){
+    console.log(!pontosDeVida-10)
+    }
+    else if(movimento==="defender"){
+        console.log(!pontosDeVida+10)
+    }
+    else if(movimento==="habilidade especial"){
+    console.log(!pontosDeVida-20)
+    }
+    else{
+        console.log("nao identificado")
+    }
+
+}
+return frodo,legolas
+}
+console.log(frodo)
+console.log(legola)
+
+function deletar(){
+    const personagemExcluido = prompt("Qual o personagem a ser excluido?")
+    if(personagemExcluido==="frodo")
+    console.log(frodo.)
+}
+
+function buscarPersonagem(){
+    const personagemBuscado = prompt("Qual personagem disponivel voce quer pesquisar?")
+    if(personagemBuscado==="Frodo")
+}
+
